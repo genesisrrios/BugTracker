35a22b1af4d8ba9ea079691cf54827584db59559
Genesis Rivera <genesisrrios@gmail.com>
2021-08-10 20:59:35 -0400

    cookie session support

--- BugTracker/Startup.cs
+++ BugTracker/Startup.cs
@@ -26,6 +26,16 @@ namespace BugTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".BugTracker.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddDbContext<BugtrackerContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("LocalHost")));
             services.AddSingleton<AuthenticationService>();
@@ -51,7 +61,7 @@ namespace BugTracker
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseSession();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
